b099a71567c7cbfe767a9151e91a6ff5362270ba
Andreas Depping <Andreas Depping@DeppingITGAIN>
2011-11-24 18:05:07 +0100

    deleted Hello Release 1.0

--- GitDemo/src/GitDemo.java
+++ GitDemo/src/GitDemo.java
@@ -5,8 +5,7 @@ public class GitDemo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello Release 1.0!");
-		System.out.println("Hallo ITGAIN");
+		System.out.println("Hello ITGAIN");
 	}
 
 }
