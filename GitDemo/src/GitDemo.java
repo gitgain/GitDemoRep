b3d93a9916cd162a03566becd0021b3e69631f24
sven <sven@LENOVO-DF582FA2>
2011-11-26 13:33:56 +0100

    test

--- GitDemo/src/GitDemo.java
+++ GitDemo/src/GitDemo.java
@@ -8,14 +8,11 @@ public class GitDemo {
 		System.out.println("Hello ITGAIN");
 		System.out.println("Bugfix123");
 		
-
 		System.out.println("Bugfix_1_2_3");
 
 		
 		System.out.println("Udo and Erik were here!");
-		System.out.println("Udo and Erik were here!");
 		System.out.println("Kay, Sven und Deborah auch!");
-
 
 	}
 
