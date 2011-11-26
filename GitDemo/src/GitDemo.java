813911e6b9723c841b0432a2b4c6c89228a2cbb4
nageler <antje.nageler@itgain.de>
2011-11-26 13:37:42 +0100

    war hier 2

--- GitDemo/src/GitDemo.java
+++ GitDemo/src/GitDemo.java
@@ -8,6 +8,11 @@ public class GitDemo {
 		System.out.println("Hello ITGAIN");
 		System.out.println("Bugfix123");
 		System.out.println("war hier JK AN");
+		
+		System.out.println("Udo and Erik were here!");
+		System.out.println("Kay, Sven und Deborah auch!");
+		
+		System.out.println("war hier 2 JK AN");
 	}
 
 }
