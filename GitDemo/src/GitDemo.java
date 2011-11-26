3b5b2c7c1e389a50f90729986a8acb2312db96c6
erik <erik@Watson>
2011-11-26 13:07:50 +0100

    Udo and Erik

--- GitDemo/src/GitDemo.java
+++ GitDemo/src/GitDemo.java
@@ -7,6 +7,8 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello ITGAIN");
 		System.out.println("Bugfix123");
+		
+		System.out.println("Udo and Erik were here!");
 	}
 
 }
