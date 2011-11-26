5a97321de17d5e56c82360bdee70a9f8172ee893
Andreas Depping <Andreas Depping@DeppingITGAIN>
2011-11-26 13:30:03 +0100

    unsere nachricht

--- GitDemo/src/GitDemo.java
+++ GitDemo/src/GitDemo.java
@@ -10,6 +10,7 @@ public class GitDemo {
 		
 		System.out.println("Udo and Erik were here!");
 		System.out.println("Kay, Sven und Deborah auch!");
+		System.out.println("COBOL kann das auch!");
 	}
 
 }
